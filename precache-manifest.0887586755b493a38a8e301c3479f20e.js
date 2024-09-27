3ff7279929843f84f23b897f82d61584de60180e
Project-Vigil-1602 <project.vigil.1602@gmail.com>
2024-09-27 23:32:21 +0530

    Updates

---
+++ precache-manifest.0887586755b493a38a8e301c3479f20e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3acc45d05ffa0612f3074a25690f785a",
+    "url": "/Verve-2024/index.html"
+  },
+  {
+    "revision": "b25c776c94f54ad3d311",
+    "url": "/Verve-2024/static/js/10.f0f1c6af.chunk.js"
+  },
+  {
+    "revision": "a4b04aa6725462030603",
+    "url": "/Verve-2024/static/js/11.ed880947.chunk.js"
+  },
+  {
+    "revision": "b03f36338418d1d37921",
+    "url": "/Verve-2024/static/js/12.edf15f09.chunk.js"
+  },
+  {
+    "revision": "2cab228d22b794476820",
+    "url": "/Verve-2024/static/js/2.951c0ead.chunk.js"
+  },
+  {
+    "revision": "248e8de01e2bbb4e192fd513a6f8c548",
+    "url": "/Verve-2024/static/js/2.951c0ead.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b0ef13f2c4aeccb9fb2",
+    "url": "/Verve-2024/static/js/3.d5deaea8.chunk.js"
+  },
+  {
+    "revision": "905c3659d3574fbe535e",
+    "url": "/Verve-2024/static/js/4.c7baeae7.chunk.js"
+  },
+  {
+    "revision": "cdf39ded2565ed769c08",
+    "url": "/Verve-2024/static/js/5.14296c92.chunk.js"
+  },
+  {
+    "revision": "443a94164a0fe7be5000",
+    "url": "/Verve-2024/static/js/6.7658bb02.chunk.js"
+  },
+  {
+    "revision": "7f6ac9493408e4dcb407",
+    "url": "/Verve-2024/static/js/7.982ce4a9.chunk.js"
+  },
+  {
+    "revision": "ee5a7e9412c78c7c9991",
+    "url": "/Verve-2024/static/js/8.129d5260.chunk.js"
+  },
+  {
+    "revision": "b190827309fc09517b81",
+    "url": "/Verve-2024/static/js/9.e3d58c67.chunk.js"
+  },
+  {
+    "revision": "a88b991533843a5dac1d",
+    "url": "/Verve-2024/static/js/main.2fd553f9.chunk.js"
+  },
+  {
+    "revision": "d363b72e4665ed203a63",
+    "url": "/Verve-2024/static/js/runtime-main.3b0e18e1.js"
+  }
+]);
